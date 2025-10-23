7003dd2bd4af9ef4d6cd6281e0f7df1feb1ad740
Supun Darshana Manathunga <dsupun916@gmail.com>
2025-10-23 16:06:31 +0530

    Refactor imports in LocationData M3.java

--- LocationData M3.java
+++ LocationData M3.java
@@ -1,8 +1,13 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
-import java.util.List;
-import java.util.ArrayList;
+
 
 class Location {
 
